7f4d0b2d6cecfa897d28194b565cbba924b24a83
Jerome Wynne <J.J.Wynne47@gmail.com>
2017-03-31 20:48:57 +0100

    Update symmetric_and_orthogonal.m

--- symmetric_and_orthogonal.m
+++ symmetric_and_orthogonal.m
@@ -1,4 +1,5 @@
 function [R, D] = symmetric_and_orthogonal(a)
+% For verifying the form of a matrix that is both symmetric and orthogonal
     A = [-a sqrt(1 - a.^2); sqrt(1 - a.^2) a];
     R = A^2
     [ignore, D] = eig(A);
