0843141f09d30ab9509b7e15b0aa63d3a2697072
Jerome Wynne <J.J.Wynne47@gmail.com>
2017-03-31 20:47:19 +0100

    Added description

--- markov_matrix.m
+++ markov_matrix.m
@@ -1,3 +1,5 @@
+% Plots the development of a vector under
+% repeated transformation by a Markov matrix
 function u = markov_matrix(A, u, n_steps, c)
     % u is a column vector, A is a square matrix
     z = [0, 0];
