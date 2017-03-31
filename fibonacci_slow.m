345947d5e435116549b72b59033c477d525275b7
Jerome Wynne <J.J.Wynne47@gmail.com>
2017-03-31 20:42:56 +0100

    Made description more meaningful

--- fibonacci_slow.m
+++ fibonacci_slow.m
@@ -1,5 +1,5 @@
 function F_n = fibonacci_slow(n)
-% Computes nth Fibonacci number via linear algebra
+% Computes nth Fibonacci number via closed solution to difference equation
     A = [1 1; 1 0];
     u_0 = [1; 0];
     u_n = (A^n)*u_0;
